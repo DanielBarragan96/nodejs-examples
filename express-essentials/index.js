5a1c84445061afe0711ed9475b94d9a036feb8fd
DanielBarragan96 <ie702554@iteso.mx>
2022-07-23 13:04:04 -0500

    routing params

--- express-essentials/index.js
+++ express-essentials/index.js
@@ -4,8 +4,19 @@ import data from "./data/mock.json" assert { type: "json" };
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+
+app.use("/images", express.static("images"));
+
 app.get("/", (req, res) => {
   res.json(data);
+});
+
+app.route("/class/:id").get((req, res) => {
+  const studentId = Number(req.params.id);
+
+  const student = data.filter((student) => student.id == studentId);
+  res.send(student);
 });
 
 app.listen(PORT, () => {
